3f8a844bf9ef3bff1ff684faba094f843c7a2d62
yubin <yubin@cn.ibm.com>
2015-12-31 13:31:54 +0800

    one thread read key and one for print it out

--- Keyinput.cpp
+++ Keyinput.cpp
@@ -2,7 +2,6 @@
 #include <string.h>
 #include <string.h>     /* strerror() */
 #include <errno.h>      /* errno */
-
 #include <fcntl.h>      /* open() */
 #include <unistd.h>     /* close() */
 #include <sys/ioctl.h>  /* ioctl() */
@@ -14,49 +13,40 @@
 #include <sys/wait.h>
 #include <sys/types.h>
 #include <iostream>
-#include <vector>
 #include <pthread.h>
+#include <deque>
+#include <time.h>
 #define EV_BUF_SIZE 16
 using namespace std;
-struct key_msg
+class key_msg
 {
-    int value;
-    time_t time;
+    public:
+        key_msg(int va,time_t tm)
+        {
+            code = va;
+            stime = tm;
+        }
+        int code;
+        time_t stime;
 };
-int main(int argc, char *argv[])
-{
-    int fd, sz;
-    unsigned i;
 
-    /* A few examples of information to gather */
+void * gather_input(void *args)
+{
+    int sz;
+    int fd;
     unsigned version;
     unsigned short id[4];                   /* or use struct input_id */
     char name[256] = "N/A";
-    vector<struct key_msg> key_msg_collector;
-    int shmid = shmget(IPC_PRIVATE,128*sizeof(struct key_msg),IPC_CREAT|0600);
-    if (shmid <0){
-        printf("error in create share memory!\n");
-        return -1;
-    }
-    struct key_msg *key_freq = (struct key_msg *)shmat(shmid,NULL,0);
-    bzero(key_freq,128*sizeof(struct key_msg ));
+    unsigned long i;
+    char *event_path = (char *)(((void **)args)[0]);
+    deque<key_msg * >* key_events_p  = (deque<key_msg *> *)(((void **)args)[1]);
     struct input_event ev[EV_BUF_SIZE]; /* Read up to N events ata time */
-
-    if (argc < 2) {
-        fprintf(stderr,
-            "Usage: %s /dev/input/eventN\n"
-            "Where X = input device number\n",
-            argv[0]
-        );
-        return EINVAL;
-    }
-
-    if ((fd = open(argv[1], O_RDONLY)) < 0) {
+    if ((fd = open(event_path, O_RDONLY)) < 0) {
         fprintf(stderr,
             "ERR %d:\n"
             "Unable to open `%s'\n"
             "%s\n",
-            errno, argv[1], strerror(errno)
+            errno, event_path, strerror(errno)
         );
     }
     /* Error check here as well. */
@@ -81,19 +71,8 @@ int main(int argc, char *argv[])
         id[ID_PRODUCT],
         id[ID_VERSION]
     );
-    pid_t pid = fork();
-    if (pid==0){
-        int status;
-        while(1){
-            sleep(1);
-            printf("%ld\n",key_freq[48]);
-        }
-        waitpid(pid,&status,0);
-    }else{
-
-    struct key_msg *key_freq = (struct key_msg *)shmat(shmid,NULL,0);
     /* Loop. Read event file and parse result. */
-    for (;;) {
+     while(1){
         sz = read(fd, ev, sizeof(struct input_event) * EV_BUF_SIZE);
 
         if (sz < (int) sizeof(struct input_event)) {
@@ -101,13 +80,13 @@ int main(int argc, char *argv[])
                 "ERR %d:\n"
                 "Reading of `%s' failed\n"
                 "%s\n",
-                errno, argv[1], strerror(errno)
+                errno, "keyboard", strerror(errno)
             );
             goto fine;
         }
-
         /* Implement code to translate type, code and value */
         for (i = 0; i < sz / sizeof(struct input_event); ++i) {
+            /*
             fprintf(stderr,
                 "%ld.%06ld: "
                 "type=%02x "
@@ -119,17 +98,54 @@ int main(int argc, char *argv[])
                 ev[i].code,
                 ev[i].value
             );
-            if(ev[i].value==1)
+            */
+            if (ev[i].value ==1)
             {
-                [ev[i].code];
-                printf("%ld\n\n\n",key_freq[48]);
+                key_msg *tmp_store = new key_msg(ev[i].code,time((time_t *)NULL));
+                key_events_p->push_back(tmp_store);
+
             }
         }
-    }
-    }
 
+    }
 fine:
-    close(fd);
+        close(fd);
+        return NULL;
+}
+int main(int argc, char *argv[])
+{
+
+    /* A few examples of information to gather */
+    deque<key_msg *> key_events;
+    pthread_t gather;
+    if (argc < 2) {
+        fprintf(stderr,
+            "Usage: %s /dev/input/eventN\n"
+            "Where X = input device number\n",
+            argv[0]
+        );
+        return EINVAL;
+    }
+    void * args[2]={argv[1],&key_events};
+    void * result;
+    pthread_create(&gather,NULL,gather_input,args);
+
+    while(1)
+    {
+        sleep(2);
+        key_msg * this_dat;
+        if (key_events.empty()==true)
+        {
+            cout<<"None Data"<<endl;
+            continue;
+        }
+        this_dat= key_events.front();
+        key_events.pop_front();
+
+        cout<<this_dat->code<<endl;
+        delete this_dat;
+    }
+    pthread_join(gather,&result);
 
     return errno;
 }
